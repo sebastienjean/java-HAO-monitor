bb83b70b36b108f536d27361f5186f0fb6fc8fdd
sebastienjean <baz.jean@gmail.com>
2013-01-21 10:26:50 +0100

    Added javadoc comments to Stoppable

--- src/fr/iutvalence/ubpe/core/interfaces/Stoppable.java
+++ src/fr/iutvalence/ubpe/core/interfaces/Stoppable.java
@@ -1,6 +1,15 @@
 package fr.iutvalence.ubpe.core.interfaces;
 
+/**
+ * Defines the general contract of a stoppable process.
+ * 
+ * @author sebastienjean
+ *
+ */
 public interface Stoppable
 {
+	/**
+	 * Notifies for termination. 
+	 */
 	public void mustStop();
 }
