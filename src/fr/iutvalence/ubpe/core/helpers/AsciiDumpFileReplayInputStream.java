e8fcb74913d025c0f611ca21958b1c06472f429d
sebastienjean <baz.jean@gmail.com>
2013-01-29 14:06:45 +0100

    Added the capability to replay capture files
    - AsciiDumpFileReplayInputStream is used as an InputStream reading text
    lines in a dump, with a given pause between lines)
    - UBPE2012ReplayRelayClientWithLocalStorage is a client-side app that
    takes dump as an input

---
+++ src/fr/iutvalence/ubpe/core/helpers/AsciiDumpFileReplayInputStream.java
@@ -0,0 +1,56 @@
+package fr.iutvalence.ubpe.core.helpers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+
+public class AsciiDumpFileReplayInputStream extends PipedInputStream implements Runnable
+{
+	private final BufferedReader in;
+		
+	private final PrintStream out;
+	
+	private final long pause;
+
+	public AsciiDumpFileReplayInputStream(File path, String charset, long pauseBetweenLines) throws IOException
+	{
+		this.in = new BufferedReader(new InputStreamReader(new FileInputStream(path), charset));
+		this.out = new PrintStream(new PipedOutputStream(this), true, charset);
+		this.pause = pauseBetweenLines;
+	}
+
+	
+	public void run()
+	{
+		while (true)
+		{	
+			try
+			{
+				String line = this.in.readLine();
+				// TODO remove debug
+				if (line == null) throw new Exception();
+				System.out.println(line);
+				this.out.println(line);
+			}
+			catch (Exception e)
+			{
+				System.out.println("End");
+				return;
+			}
+			
+			try
+			{
+				Thread.sleep(this.pause);
+			}
+			catch (InterruptedException e)
+			{
+				// Ignoring it
+			}		
+		}
+	}
+}
